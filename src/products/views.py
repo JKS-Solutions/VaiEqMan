f0045ecfc3bc388dd1716f6c585b5ef9121f52da
Kim Jämsen <jamsen.kim@gmail.com>
2019-10-27 23:28:29 +0200

    Codereview fixes
    MODIFIED
    > products/views.py
    - fixed stupid list handling
    > list.html
    -  added links to models
    > one.html
    - fixed list stupidity and modified th for clarity

--- src/products/views.py
+++ src/products/views.py
@@ -58,10 +58,7 @@ def prodct_removeSubcomponent(model_id, target_id):
 @app.route("/models/<model_id>/", methods=["GET"])
 def get_product(model_id):
     p = Product.query.get(model_id)
-    subC = []
-    for x in p.subcomponents:
-        subC.append(x)
-    return render_template("models/one.html", product = p, manufacturers = Manufacturer.query.all(), subcomponents = subC)
+    return render_template("models/one.html", product = p, manufacturers = Manufacturer.query.all())
 
 @app.route("/models/", methods=["POST"])
 @login_required
